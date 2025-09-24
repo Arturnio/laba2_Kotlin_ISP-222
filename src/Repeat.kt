4d71ce4a9c56603071b0f38e8e7da2c0339c5459
Artur K <only_up_ak@gmail.com>
2025-09-24 16:24:22 +0300

    new commit 2

---
+++ src/Repeat.kt
@@ -0,0 +1,9 @@
+fun main(){
+println("Начинаем ритуал защиты")
+    var index = 0
+    repeat(5){
+        println("Заклинание защиты активировано!(шаг$index)")
+        index++
+    }
+    println("Замок защищен!")
+}
